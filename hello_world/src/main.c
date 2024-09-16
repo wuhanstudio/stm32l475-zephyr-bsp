8081053eb6f9e1b5c6376ef6d1b7b00eb655ad53
wuhanstudio <wuhanstudio@qq.com>
2024-09-16 19:11:37 +0100

    Add LVGL

--- hello_world/src/main.c
+++ hello_world/src/main.c
@@ -5,10 +5,40 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/display.h>
+
+#include <lvgl.h>
+#include "lvgl_sample.h"
+
+
+#define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(app);
 
 int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+
+	const struct device *display_dev;
+
+	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+	if (!device_is_ready(display_dev)) {
+		LOG_ERR("Device not ready, aborting test");
+		return 0;
+	}
+
+	lv_sample();
+	lv_task_handler();
+	display_blanking_off(display_dev);
+
+	while (1) {
+		lv_task_handler();
+		k_sleep(K_MSEC(10));
+	}
 
 	return 0;
 }
