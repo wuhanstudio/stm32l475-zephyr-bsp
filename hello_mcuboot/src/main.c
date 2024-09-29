5ddb4c74ab1c64e24bffd73af17c98689fc0b182
wuhanstudio <wuhanstudio@qq.com>
2024-09-29 09:07:26 +0100

    [mcuboot] print firmware version

--- hello_mcuboot/src/main.c
+++ hello_mcuboot/src/main.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
 
-	const char *buildString = "This build " CONFIG_BOARD_TARGET " was compiled at " __DATE__ ", " __TIME__ ".";
+	const char *buildString = "This build " CONFIG_MCUBOOT_IMGTOOL_SIGN_VERSION " was compiled at " __DATE__ ", " __TIME__ ".";
 	printf("%s\n", buildString);
 
 	return 0;
