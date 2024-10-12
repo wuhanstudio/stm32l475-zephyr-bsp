562ca71a52e22713fbb27f5fbc67a0413d611433
Han Wu <wuhanstudios@gmail.com>
2024-10-12 18:30:42 +0100

    [telnet] Add telnet

--- hello_at_wifi/src/main.c
+++ hello_at_wifi/src/main.c
@@ -1,14 +1,30 @@
 /*
- * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ * Copyright (c) 2017 Intel Corporation.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(net_telnet_sample, LOG_LEVEL_DBG);
+
+#include <zephyr/kernel.h>
+#include <zephyr/linker/sections.h>
+#include <errno.h>
 #include <stdio.h>
+
+#include <zephyr/net/net_core.h>
+#include <zephyr/net/net_if.h>
+#include <zephyr/net/net_mgmt.h>
+
+#include "net_sample_common.h"
 
 int main(void)
 {
-	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+	LOG_INF("Starting Telnet sample");
+
+	wait_for_network();
+
+	LOG_INF("Network Connected");
 
 	return 0;
 }
