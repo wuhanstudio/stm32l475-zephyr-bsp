94318140fd1a98daa658e2f7e974949eeeb2d32a
wuhanstudio <wuhanstudio@qq.com>
2024-11-09 15:17:03 +0000

    [USB CDC] Update shell and console devices

--- hello_usb_cdc/src/main.c
+++ hello_usb_cdc/src/main.c
@@ -12,12 +12,11 @@
 #include <zephyr/usb/usbd.h>
 #include <zephyr/drivers/uart.h>
 
-BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
+BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_shell_uart), zephyr_cdc_acm_uart),
 	     "Console device is not ACM CDC UART device");
 
 int main(void)
 {
-	// const struct device *const dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
     const struct device *const dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_shell_uart));
 	uint32_t dtr = 0;
 
